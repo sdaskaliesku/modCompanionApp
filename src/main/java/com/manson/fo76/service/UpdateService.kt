a846da4d62f385c467acb59382d5a478c4efc041
sdaskaliesku <manson.serg@gmail.com>
2020-09-28 02:17:11 +0300

    Update token; It's readonly with permissions to read packages, it's ssafe to expose it

--- src/main/java/com/manson/fo76/service/UpdateService.kt
+++ src/main/java/com/manson/fo76/service/UpdateService.kt
@@ -10,7 +10,8 @@ import javax.ws.rs.core.MediaType
 class UpdateService(objectMapper: ObjectMapper) : BaseRestClient(objectMapper) {
 
     companion object {
-        const val TOKEN = "671446c1fd8f2e2736073ca04d02cd401ceb20a0"
+        const val TOKEN_1 = "804b7dc76a58bf533"
+        const val TOKEN_2 = "abe98983e12587484063e94"
     }
 
     fun getLatestVersion(): LatestVersion {
@@ -19,7 +20,7 @@ class UpdateService(objectMapper: ObjectMapper) : BaseRestClient(objectMapper) {
                 .target("https://api.github.com/graphql")
         val query = "{repository(owner:\"sdaskaliesku\",name:\"modCompanionApp\"){packages(first:1){nodes{latestVersion{version}}}}}"
         val map = mapOf("query" to query)
-        val response = webResource.request().header("Authorization", "bearer $TOKEN").accept(MediaType.APPLICATION_JSON).buildPost(Entity.entity(map, MediaType.APPLICATION_JSON)).invoke()
+        val response = webResource.request().header("Authorization", "bearer $TOKEN_1$TOKEN_2").accept(MediaType.APPLICATION_JSON).buildPost(Entity.entity(map, MediaType.APPLICATION_JSON)).invoke()
         return response.readEntity(DummyGraphQLResponse::class.java).data.repository.packages.nodes[0].latestVersion
     }
 
