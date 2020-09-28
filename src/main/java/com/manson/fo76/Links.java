5ff8b6d0384eb192a028b7f2566cbb720272a84d
sdaskaliesku <manson.serg@gmail.com>
2020-09-28 04:17:53 +0300

    Added nexus page link

--- src/main/java/com/manson/fo76/Links.java
+++ src/main/java/com/manson/fo76/Links.java
@@ -4,7 +4,6 @@ public class Links {
 
   public static final String GITHUB = "https://github.com/sdaskaliesku/modCompanionApp";
   public static final String DISCORD = "https://discord.gg/7fef733";
-//  TODO: fill-in, once app will be published on nexus
-  public static final String NEXUS = "";
+  public static final String NEXUS = "https://www.nexusmods.com/fallout76/mods/744/";
 
 }
